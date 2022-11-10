77b8211135e176be63ba443e43f51600e8dfe91f
duvan1026 <duvancastro1026@gmail.com>
2022-11-10 16:29:14 -0500

    Clase 44: Evento Click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,32 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ( { value }) => {
+
+    /* Funcion con sintaxis normal*/  
+    // function handleAdd(event, newValue) { 
+    //     // console.log(event);
+    //     console.log(newValue);
+    // }
+    /** para llamar la funcion .... (event) => handleAdd(event, 'hola') */
+
+    /* Funcion de flecha equivalente a la funcion normal*/
+    const handleAdd = (event) => { 
+        console.log(event);
+        //console.log(newValue);
+    }
+    /* como en este caso solo se requiere el valor de event 
+    *  (event) => handleAdd(event)
+    *  se establece solo con el nombre de la funcion */
     
     return(
         <>
            <h1>CounterApp</h1> 
-           <p>{ value }</p>
+           <h2> { value } </h2>
+
+           {/* https://es.reactjs.org/docs/events.html  Documentacion REACT Eventos */}
+           <button onClick={ handleAdd }>
+                +1
+           </button>
         </>
     )
 }
