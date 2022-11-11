4403814c68bf338d8443879d04b86e6217f441da
duvan1026 <duvancastro1026@gmail.com>
2022-11-11 13:39:06 -0500

    CounterApp.jsx: useState Hook, clase 45

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,6 +1,19 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export const CounterApp = ( { value }) => {
+
+    /** Hooks */
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = () => {
+
+        //setCounter( counter + 1); // forma 1 de cambiar valor counter
+        setCounter( (c) => c + 1  ); // forma 2 para cambiar valor counter 
+
+    }
+
+
 
     /* Funcion con sintaxis normal*/  
     // function handleAdd(event, newValue) { 
@@ -10,10 +23,10 @@ export const CounterApp = ( { value }) => {
     /** para llamar la funcion .... (event) => handleAdd(event, 'hola') */
 
     /* Funcion de flecha equivalente a la funcion normal*/
-    const handleAdd = (event) => { 
-        console.log(event);
-        //console.log(newValue);
-    }
+    // const handleAdd = (event) => { 
+    //     console.log(event);
+    //     //console.log(newValue);
+    // }
     /* como en este caso solo se requiere el valor de event 
     *  (event) => handleAdd(event)
     *  se establece solo con el nombre de la funcion */
@@ -21,7 +34,7 @@ export const CounterApp = ( { value }) => {
     return(
         <>
            <h1>CounterApp</h1> 
-           <h2> { value } </h2>
+           <h2> { counter } </h2>
 
            {/* https://es.reactjs.org/docs/events.html  Documentacion REACT Eventos */}
            <button onClick={ handleAdd }>
