94f03da544b78467ea12a091d7d3d9b050ebbfd3
duvan castro <duvancastro1026@gmail.com>
2022-11-29 19:09:33 -0500

    Prueba test 08

---
+++ tests/base_pruebas/08-imp-exp.test.js
@@ -0,0 +1,19 @@
+//import { getSaludo } from "../../src/base-pruebas/02-template-string"
+
+
+describe('Pruebas en 08-imp-exp.test', () => { 
+
+    test('getHeroeById debe retornar un heroe por ID', () => {
+
+        //Objeto
+        const id = 1;
+        const hero = getHeroeById( id );
+        console.log(hero)
+
+        //const user = getUser();
+
+        //Comparar entre dos objetos
+        //expect( testUser ).toEqual( user );
+      
+    });
+ });
